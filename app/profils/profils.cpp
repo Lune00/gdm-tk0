f86f777afa716bf16169618617886ceb7a345873
paul schuhmacher <paul.schuhmacher@umontpellier.fr>
2017-03-14 18:10:46 +0100

    Profil moyen - copie executable profils - input set

--- app/profils/profils.cpp
+++ app/profils/profils.cpp
@@ -9,9 +9,52 @@
 #include "dataSet.hpp"
 
 
-int main ()
+
+
+
+
+
+
+void profilMoyenVitesse(unsigned int Nbins,unsigned int Nstart,unsigned int Nend)
 {
-	cout<<"Hello world."<<endl;
+if( Nbins == 0 || (Nstart>Nend) ) {
+	cerr<<"@profilMoyenVitesse erreur parametres."<<endl;
+	return;
+}
+
+
+
+}
+int main (int argc,char **argv)
+{
+
+
+	bool calcProfilVitesse = false;
+	unsigned int NbinsV = 0 ;
+	unsigned int Nstart = 0 ;
+	unsigned int Nend = 0 ;
+
+	string token;
+
+	ifstream is(argv[1]);
+
+	is>>token;
+	while(is)
+	{
+		if(token=="vitesse"){
+			calcProfilVitesse = true ;
+			is>>NbinsV;
+			is>>Nstart;
+			is>>Nend;
+		}
+		is>>token;
+	}
+
+	if(calcProfilVitesse) {
+		cout<<".Calcul profil moyen de vitesse - Bins : "<<NbinsV<<endl;
+		profilMoyenVitesse(NbinsV,Nstart,Nend);
+	}
+
 
 	return 0;
 }
