3897857e2b19ee672e3a494f76b2ffa2632ae710
paul schuhmacher <paul.schuhmacher@umontpellier.fr>
2017-03-21 19:29:36 +0100

    Ajout script convertir ps to png / reminder ffmpeg basic

---
+++ pstopng.sh
@@ -0,0 +1,4 @@
+folder_ps=Analyse/PS1
+folder_png=png
+mkdir -p $folder_png ;
+for i in $(ls $folder_ps/*ps) ; do  name_png=$(echo $i | cut -f 1 -d '.').png ; echo "$i to $name_png" ; convert -density 10 $i $name_png  ; mv $name_png $folder_png ; done
