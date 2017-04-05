34f9ec08df3afbbf63e2fcf0ff7b2fe8d013b158
paul schuhmacher <paul.schuhmacher@umontpellier.fr>
2017-04-05 19:00:50 +0200

    Lecture fichier configuration ok - Initialisation grid avec donnees issues du gdm

--- app/continu/continu.cpp
+++ app/continu/continu.cpp
@@ -7,8 +7,15 @@
 #include "simulation.hpp"
 #include "system.hpp"
 #include "dataSet.hpp"
-#include<algorithm>
+#include <algorithm>
 
+struct Globals{
+	int nx ;
+	int ny ;
+	int istart, iend, di;
+	double xmin, xmax, ymin, ymax ;
+	string metrics;
+};
 
 class Point{
 
@@ -33,48 +40,24 @@ class Grid{
 	public:
 		Grid(){};
 		~Grid();
-		Grid(unsigned int, unsigned int);
+		Grid(Globals);
 		double getX(int,int);
 		double getY(int,int);
-		void initMetrics(double,double,double,double);
 		void writeGrid(string);
 };
 
 //Ajouter un fichier a lire poux les min,max et set tous les paramètres
-Grid::Grid(unsigned int nx , unsigned int ny)
+Grid::Grid(Globals parametres)
 {
-	nx_ = nx ;
-	ny_ = ny ;
+	nx_ = parametres.nx ;
+	ny_ = parametres.ny ;
+
+	xmin_ = parametres.xmin ;
+	xmax_ = parametres.xmax ;
+	ymin_ = parametres.ymin ;
+	ymax_ = parametres.ymax ;
 
 	array = new Point [ nx_ * ny_ ];
-
-	xmin_ = 0. ;
-	ymin_ = 0. ;
-	xmax_ = 0. ;
-	ymax_ = 0. ;
-	dx_ = 0. ;
-	dy_ = 0. ;
-}
-Grid::~Grid()
-{
-	delete[] array;
-}
-
-double Grid::getX(int i,int j)
-{
-	return array[ i * ny_ + j].getX();
-}
-
-double Grid::getY(int i,int j)
-{
-	return array[ i * ny_ + j].getY();
-}
-void Grid::initMetrics(double xmin, double xmax, double ymin, double ymax)
-{
-	xmin_ = xmin ;
-	xmax_ = xmax ;
-	ymin_ = ymin ;
-	ymax_ = ymax ;
 
 	if(xmin_ > xmax_ || ymin_ > ymax_)
 	{
@@ -108,7 +91,20 @@ void Grid::initMetrics(double xmin, double xmax, double ymin, double ymax)
 		x = xmin_;
 	}
 	cerr<<"Metrics done."<<endl;
+}
+Grid::~Grid()
+{
+	delete[] array;
+}
 
+double Grid::getX(int i,int j)
+{
+	return array[ i * ny_ + j].getX();
+}
+
+double Grid::getY(int i,int j)
+{
+	return array[ i * ny_ + j].getY();
 }
 
 
@@ -136,25 +132,78 @@ void Grid::writeGrid(string filename)
 	gridout.close();
 }	
 
-void initContinu(ifstream& is)
+
+void readMetrics(string file, Globals& parametres)
+{
+	ifstream is(file);
+	string token;
+	is >> token;
+	while(is)
+	{
+		if(token=="xmin") is >> parametres.xmin;
+		if(token=="xmax") is >> parametres.xmax;
+		if(token=="ymin") is >> parametres.ymin;
+		if(token=="ymax") is >> parametres.ymax;
+		is >> token;
+	}
+}
+		
+void initContinu(ifstream& is,Globals& parametres)
 {
 	if(!is)
 	{
-		cerr << "@Simulation::read_data, cannot open file " << endl;
+		cerr << "@initContinu : cannot open file " << endl;
 		return;
 	}
-	string token ;
 
+	string token ;
+	is >> token ;
+	while(is)
+	{
+		if(token=="Grid{")
+		{
+			is >> token;
+			while(is)
+			{
+				if(token=="nx") is >>parametres.nx ; 
+				if(token=="ny") is >>parametres.ny ; 
+				if(token=="metrics") 
+				{
+					is >> token;
+					readMetrics(token,parametres);
+				}
+				else if (token=="}") break;
+				is >> token;
+			}
+		}
+		if(token=="Data{")
+		{
+			is >> token;
+			while(is)
+			{
+				if(token=="ini") is >>parametres.istart ; 
+				if(token=="end") is >>parametres.iend ; 
+				if(token=="di") is >>parametres.di; 
+				else if (token=="}") break;
+				is >> token;
+			}
+		}
+
+		is >> token;
+	}
 }
 
 int main (int argc,char **argv)
 {
+	Globals parametres;
 	ifstream is(argv[1]);
-	initContinu(is);
-
-	//Faire une fonction qui lit un fichier de parametres
-	Grid grid(128,128);
-	grid.initMetrics(0.,10.,0.,10.);
+	initContinu(is,parametres);
+	Grid grid(parametres);
+	cerr<<"nx = "<<parametres.nx<<" ny = "<<parametres.ny<<endl;
+	cerr<<"Init metrics file : "<<parametres.metrics<<endl;
+	cerr<<"xmin = "<<parametres.xmin<<" xmax = "<<parametres.xmax<<endl;
+	cerr<<"ymin = "<<parametres.ymin<<" ymax = "<<parametres.ymax<<endl;
+	//Initialisation a partir du fichier
 	grid.writeGrid("grid.txt");
 	return 0;
 }
