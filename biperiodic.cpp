b13f23b1f27f63ec25fcd21d1c00ee3102bc66df
Paul Schuhmacher <paul.schuhmacher@laposte.net>
2017-11-20 18:44:44 +0100

    up bip

--- biperiodic.cpp
+++ biperiodic.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<cmath>
 #include<fstream>
+#include<vector>
 
 using namespace std;
 
@@ -15,6 +16,10 @@ class Tensor2x2{
 		Tensor2x2(): xx_(0.0), xy_(0.0),yx_(0.0), yy_(0.0){};
 		Tensor2x2(double xx, double xy, double yx, double yy): xx_(xx), xy_(xy), yx_(yx), yy_(yy){};
 		~Tensor2x2(){};
+		
+		void set(double xx, double xy, double yx, double yy);
+		double getDet(){ return (xx_*yy_ - xy_*yx_) ;}
+
 		//Accesseurs:
 		double getxx() const { return xx_;} 
 		double getxy() const { return xy_;} 
@@ -22,7 +27,12 @@ class Tensor2x2{
 		double getyy() const { return yy_;} 
 };
 
-
+void Tensor2x2::set(double xx, double xy, double yx, double yy){
+	xx_ = xx;
+	xy_ = xy;
+	yx_ = yx;
+	yy_ = yy;
+}
 
 class Vecteur{
 	private:
@@ -56,24 +66,38 @@ Vecteur Vecteur::productTensor(Tensor2x2& T){
 
 class Particle{
 	private:
-		//Position:
+		//Position absolue
 		Vecteur r_;
+		//Reduced coordinate
+		Vecteur s_;
 		//Vitesse:
 		Vecteur v_;
 		//Acceleration:
 		Vecteur a_;
 	public:
 		Particle(): r_(), v_() {}; 
-		Particle(Vecteur r,Vecteur v) {r_ = r; v_ = v;}
+		Particle(Vecteur r, double L, Vecteur v) {r_ = r; v_ = v; calculReduced(L);}
 		~Particle(){};
 		void updateSimple(double dt);
 		Vecteur& getR() { return r_;}
 		void write(ofstream&);
+		void calculReduced(double L);
+		void affiche();
 };
+
+//sx et sy doivent etre compris entre 0 et 1 (initialiement dans la cellule)
+void Particle::calculReduced(double L){
+	s_.update(r_.getx() / L,r_.gety() / L);
+}
 
 void Particle::write(ofstream& of){
 	of<< r_.getx()<<" "<<r_.gety()<<endl;
 }
+
+void Particle::affiche(){
+	cout<<"Coordonnees absolues: "<<r_.getx()<<" "<<r_.gety()<<endl;
+	cout<<"Coordonnees reduites: "<<s_.getx()<<" "<<s_.gety()<<endl;
+}	
 
 
 //Euler methode: update acceleration, vitesse and position
@@ -88,7 +112,9 @@ void Particle::updateSimple(double dt){
 class Cell{
 
 	private:
+		//Dimension:
 		double L_;
+		//Coordonnees centre:
 		double xc_;
 		double yc_;
 		//Metrics: collective degrees of freedom
@@ -96,9 +122,16 @@ class Cell{
 		Tensor2x2 hdot;
 	public:
 		Cell();
-		Cell(double L): L_(L),xc_(L/2.),yc_(L/2.){;}
+		Cell(double L): L_(L),xc_(L/2.),yc_(L/2.){initAffine();}
 		~Cell();
+
+		//Met a jours la periodicite 
 		void PeriodicBoundaries(Particle&);
+		//Distorsion cellule sur un pas de temps(calcul champ homogene)
+		void distorsion(double dt);
+		void initAffine();
+		//Renvoie le volume de la cellule
+		double getVolume();
 		double getL() const { return L_;}
 		double getL2() const { return L_/2.;}
 };
@@ -109,7 +142,31 @@ Cell::Cell(){
 	yc_ = 0.5 * L_ ;
 }
 
+
+//Init h et hdot
+void Cell::initAffine(){
+	//Cellule initiale:
+	h.set(L_,0.,0.,L_);
+	//Taux de deformation de la cellue:
+	hdot.set(1.,0.,0.,1.);
+}
+
+//Le volume est donné par det(h) (deux vecteurs de base de la cellule)
+double Cell::getVolume(){
+	return h.getDet();
+}
+	
+
 Cell::~Cell(){};
+
+//Applique le deplacement affine
+void Cell::distorsion(double dt){
+
+
+
+
+}
+
 
 //PeriodicBoundary Conditions
 void Cell::PeriodicBoundaries(Particle& p){
@@ -126,23 +183,36 @@ void Cell::PeriodicBoundaries(Particle& p){
 }
 
 
+void verletalgo(Cell& cell,std::vector<Particle> ps,double dt){
+
+
+}
+
 int main(){
 
 	//Initialisation:
 	double const L = 1.;
 	double dt = .05 ;
 	Cell cell(L);
-	Vecteur r0(L/2,L/2);
-	Vecteur v0(1.,0.5);
-	Particle p(r0,v0);
+
+	Vecteur r0(L/3,L/3);
+	Vecteur v0(0.,0.);
+	//Initialise coordonnees reduites, absolues, vitesse fluctuante
+	Particle p(r0,L,v0);
+	p.affiche();
 
 	ofstream tmp("particle.txt");
 	//Kinetics Time integration
-	for(int t = 0; t < 1000; t++){
-		//Update position:
-		p.updateSimple(dt);
+	for(int t = 0; t < 10; t++){
 		//Check for boundary:
 		cell.PeriodicBoundaries(p);
+		//Update position:
+		//Calcul du champ homogene: ie def cellule
+		cell.distorsion(dt);
+		//Application champ aux particules:
+
+		//Print:
+		cout<<cell.getVolume()<<endl;
 		p.write(tmp);
 	}
 	tmp.close();
